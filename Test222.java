7ea245898fe4113c714893515c6f996e0397718b
pengweizhong <pengwz@hotmail.com>
2020-04-10 17:35:12 +0800

    new file test222

---
+++ Test222.java
@@ -0,0 +1,17 @@
+public class Dev{
+	
+	public static void main(String[] args){
+		System.out.println("Hello ,Dev !");
+	}
+	
+	
+	
+	private void test(){
+		
+	}
+	
+	
+	private void test2(){
+		System.out.println("Hello ,Dev!");
+	}
+}
